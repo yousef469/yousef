32f087d206d603ae3d939cc209152f861fd57497
yousef469 <youseflovemessi41@gmail.com>
2025-11-16 09:40:34 +0000

    Add Vite config for simple-peer optimization

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['simple-peer'],
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
   server: {
     port: 3000,
     open: true
